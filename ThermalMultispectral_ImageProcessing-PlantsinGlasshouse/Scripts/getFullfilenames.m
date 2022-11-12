0a99b481b9cfbd5282704c046fe42909636ea6a5
SmartSense-iHub <117883523+SmartSense-iHub@users.noreply.github.com>
2022-11-12 15:58:25 +1100

    Delete ThermalMultispectral_ImageProcessing-PlantsinGlasshouse/Scripts directory

--- ThermalMultispectral_ImageProcessing-PlantsinGlasshouse/Scripts/getFullfilenames.m
+++
@@ -1,9 +0,0 @@
-function fullFileNames = getFullfilenames(folder_name,bandName,imageFormat)
-
-myDir_sequioa = dir(convertCharsToStrings(folder_name)+'\**\*'+bandName+'*.'+imageFormat);
-FilesName = {myDir_sequioa.name}';
-FolderName={myDir_sequioa.name}';
-fullFileNames = strcat({myDir_sequioa.folder},'\',{myDir_sequioa.name});
-fullFileNames=(fullFileNames)';
-
-end 
